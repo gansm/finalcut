b4ce9a1896ca15a65f4177d42aa63e82924c10e2
Markus Gans <guru@server.home.net>
2015-08-10 00:56:32 +0200

    Makefile.in

--- build.sh
+++ build.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
+$PREFIX="/usr"
+
 case "$1" in
   "--help"|"help")
     echo "Usage: $0 {help|debug|release}"
     exit 1
     ;;
   "--debug"|"debug")
-    ./configure CPPFLAGS="-DDEBUG" CXXFLAGS="-g -O0 -DDEBUG -W -Wall -pedantic"
+    ./configure --prefix="$PREFIX" CPPFLAGS="-DDEBUG" CXXFLAGS="-g -O0 -DDEBUG -W -Wall -pedantic"
     ;;
   "--profile"|"profile")
-    ./configure CPPFLAGS="-DDEBUG" CXXFLAGS="-pg -O0 -DDEBUG -W -Wall -pedantic"
+    ./configure --prefix="$PREFIX" CPPFLAGS="-DDEBUG" CXXFLAGS="-pg -O0 -DDEBUG -W -Wall -pedantic"
     ;;
   "--release"|"release"|*)
-    ./configure
+    ./configure --prefix="$PREFIX"
     ;;
 esac
 
