3cdaff6fe0c9eaa33c13fec500a0a5d3a2009f2b
Markus Gans <guru@server.home.net>
2015-08-10 21:50:27 +0200

    Makefile.in

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ $PREFIX="/usr"
 
 case "$1" in
   "--help"|"help")
-    echo "Usage: $0 {help|debug|release}"
+    echo "Usage: $0 {help|debug|profile|gcov|release}"
     exit 1
     ;;
   "--debug"|"debug")
@@ -12,6 +12,9 @@ case "$1" in
     ;;
   "--profile"|"profile")
     ./configure --prefix="$PREFIX" CPPFLAGS="-DDEBUG" CXXFLAGS="-pg -O0 -DDEBUG -W -Wall -pedantic"
+    ;;
+  "--gcov"|"gcov")
+    ./configure --prefix="$PREFIX" CXXFLAGS="-fprofile-arcs -ftest-coverage"
     ;;
   "--release"|"release"|*)
     ./configure --prefix="$PREFIX"
