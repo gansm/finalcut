26cd5118e6d6904934aa399e0ed5c4711d0ff29d
Markus Gans <guru@server.home.net>
2018-09-20 04:47:51 +0200

    Added pkg-config file finalcut.pc

--- scripts/kcachegrind.sh
+++ scripts/kcachegrind.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-LD_LIBRARY_PATH=../src/.libs/ valgrind --tool=callgrind -v ../examples/.libs/ui 2>/dev/null
+if [ -z "$1" ]
+then
+  PROG="../examples/.libs/ui"
+else
+  PROG="$1"
+  shift
+fi
+
+# Is the file executable?
+test ! -x "$PROG" && echo "No executable file not found" && exit -1
+
+
+LD_LIBRARY_PATH=../src/.libs/ valgrind --tool=callgrind -v "$PROG" 2>/dev/null
 kcachegrind
 rm -f callgrind.out.*
 
