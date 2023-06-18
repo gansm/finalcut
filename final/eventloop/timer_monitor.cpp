1363fe253f7f38b7815814b27eb6d9e5d62c482f
Markus Gans <guru@server.home.net>
2023-06-18 02:38:51 +0200

    Add reference

--- final/eventloop/timer_monitor.cpp
+++ final/eventloop/timer_monitor.cpp
@@ -79,7 +79,7 @@ std::mutex timer_nodes_mutex{};
 //----------------------------------------------------------------------
 void onSigAlrm (int, siginfo_t* signal_info, void*)
 {
-  const timer_t timer_id{*static_cast<const timer_t*>(signal_info->si_value.sival_ptr)};
+  const auto& timer_id = *static_cast<const timer_t*>(signal_info->si_value.sival_ptr);
   std::lock_guard<std::mutex> lock_guard(timer_nodes_mutex);
 
   for (const auto& timer_node : timer_nodes)
